52e1bb41f4d044528033ea1c9efa48f809075244
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-06-06 16:30:37 +0200

    fix snack 1

--- script.js
+++ script.js
@@ -4,10 +4,12 @@
 Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}*/
 
 function getPostTitle(id) {
-    return fetch(`https://dummyjson.com/posts/${id}`)
-        .then(response => response.json())
-        .then(post => resolve(post.title))
-        .catch(reject)
+    return new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(post => resolve(post.title))
+            .catch(reject)
+    });
 }
 
 getPostTitle(1)
@@ -20,7 +22,7 @@ getPostTitle(1)
 function lanciaDado() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            let numero = Math.floor(Math.random() * 6) + 1
+            const numero = Math.floor(Math.random() * 6) + 1
             if (Math.random() < 0.2) {
                 reject('il dado si è incastrato');
             } else {
@@ -32,5 +34,6 @@ function lanciaDado() {
 
 lanciaDado()
     .then(numero => console.log(numero))
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
+
 
