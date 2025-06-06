7e616b0c16070ac1303853dab9ccc9bd6628029f
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-06-06 15:19:27 +0200

    complete snacks 1, 2

---
+++ script.js
@@ -0,0 +1,35 @@
+
+/* 1. Ottieni il titolo di un post con una Promise.
+
+Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}*/
+
+function getPostTitle(id) {
+    return fetch(`https://dummyjson.com/posts/${id}`)
+        .then(response => response.json())
+        .then(post => post.title);
+}
+
+getPostTitle(1)
+    .then(title => console.log(title))
+    .catch(error => console.error(error));
+
+
+/*2. Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.*/
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let numero = Math.floor(Math.random() * 6) + 1
+            if (Math.random() < 0.2) {
+                reject('il dado si è incastrato');
+            } else {
+                resolve(`numero generato: ${numero}`)
+            }
+        }, 3000)
+    });
+}
+
+lanciaDado()
+    .then(numero => console.log(numero))
+    .catch(error => console.log(error))
+
